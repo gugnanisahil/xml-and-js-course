36a5638e41868617b2d0da172a38633b58a32851
gugnanisahil <90474433+gugnanisahil@users.noreply.github.com>
2022-03-24 22:00:41 -0400

    week-10 activity

---
+++ week-10/assignments/activity.js
@@ -0,0 +1,38 @@
+
+const http = require("http");
+
+const person = require("./controllers/people");
+
+const parseParams = (searchParams) => {
+    const params = new URLSearchParams(searchParams);
+    return Array.from(params.entries()).reduce(
+        (acc, [key, value]) => ({
+            ...acc,
+            [key]: value,
+        }),
+        {}
+    );
+};
+const server = http.createServer(async (req, res) => {
+    const [basePath, searchParam] = req.url.split('?');
+    if(basePath === '/api/people'){
+        const {code, data} = await person.getAll(parseParams(searchParam));
+        res.writeHead(code);
+        res.end(JSON.stringify(data));
+    }else if(req.url.match(/\/api\/people\/\w+/)){
+        const urlElements = req.url.split('/');
+        const id = urlElements[urlElements.length - 1];
+
+        const {code, data} = await person.getById(id);
+        res.writeHead(code);
+        res.end(JSON.stringify(data));
+    }
+    else{
+        res.writeHead(404);
+        res.end(JSON.stringify({ message: 'Route not found'}))
+    }
+});
+
+const PORT = 8080;
+
+server.listen(PORT, () => console.log('Server is runnin on port ${PORT}`'));
